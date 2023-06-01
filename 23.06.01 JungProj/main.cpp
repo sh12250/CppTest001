61a3e35e9f54051e145a9f0698a240cbeedfe48a
DESKTOP-O5TLUIQ\KGA <pie267925@gmail.com>
2023-06-01 17:53:59 +0900

    Fix. JungProj/main.cpp 정사각형 안에 마름모 모양 빈공간 만들어봄

--- 23.06.01 JungProj/main.cpp
+++ 23.06.01 JungProj/main.cpp
@@ -67,6 +67,106 @@ void SquareMaker(int squareSize)
 	}
 }
 
+void SpecialSquare(int size) 
+{
+	if (size % 2 == 0) 
+	{
+		size -= 1;
+	}
+
+	int space = 1;
+	int save = 1;
+
+	int widthLeft = (size - space) / 2;
+	int widthRight = widthLeft;
+	int lengthTop = (size - 1) / 2;
+	int lengthBottom = lengthTop;
+
+	while (lengthTop > 0)
+	{
+		widthLeft = (size - space) / 2;
+		widthRight = widthLeft;
+
+
+		while (widthLeft > 0)
+		{
+			printf("* ");
+			widthLeft -= 1;
+		}
+
+		if (widthLeft == 0)
+		{
+			while (space > 0)
+			{
+				printf("  ");
+				space -= 1;
+			}
+
+			if (space == 0) {
+				save += 2;
+				space = save;
+			}
+		}
+
+		while (widthRight > 0)
+		{
+			printf("* ");
+			widthRight -= 1;
+
+			if (widthRight == 0) {
+				printf("\n");
+			}
+		}
+
+		lengthTop -= 1;
+	}
+
+	printf("\n");
+
+	while (lengthBottom > 0) 
+	{
+		save -= 2;
+		space = save;
+		widthLeft = (size - save) / 2;
+		widthRight = widthLeft;
+
+
+		while (widthLeft > 0)
+		{
+			printf("* ");
+			widthLeft -= 1;
+		}
+
+		if (widthLeft == 0)
+		{
+			while (space > 0)
+			{
+				printf("  ");
+				space -= 1;
+			}
+
+			if (space == 0) {
+				if (save != size)
+				{
+					space = save;
+				}
+			}
+		}
+
+		while (widthRight > 0)
+		{
+			printf("* ");
+			widthRight -= 1;
+
+			if (widthRight == 0) {
+				printf("\n");
+			}
+		}
+
+		lengthBottom -= 1;
+	}
+}
+
 int main() 
 {
 
@@ -74,7 +174,8 @@ int main()
 
 	scanf_s("%d", &loopCount);
 
-	SquareMaker(loopCount);
+	//SquareMaker(loopCount);
+	SpecialSquare(loopCount);
 
 
 
