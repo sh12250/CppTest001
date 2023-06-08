e47871e009babaaa967cefb7c0e76565fcb89cc3
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-08 18:02:49 +0900

    Feat.이중 배열 실습

--- 23.06.08 JungProj/main.cpp
+++ 23.06.08 JungProj/main.cpp
@@ -8,29 +8,117 @@
 
 using namespace std;
 
-void Shuffle(int* ptrArr, int arrSize, int shuffleCount);
-void ShuffleOnce(int* firstNumber, int* secondNumber);
-void ArrayFunc(int* ptrArr, int arrSize);
-void ArrayFunc2(int array_[], int arrSize);
-
 // 5개 짜리 배열 만들어서
 // 유저한테 램덤하게 5개의 숫자를 입력 받아서 배열에 저장하는 거
 // 제일 쉬운 정렬은 선택 정렬
-void SelecttionSort(int* ptrArr, int arrSize)
+void SelecttionSort(int* ptrArr, int arrSize);
+void BubbleSort(int* ptrArr, int arrSize);
+void InsertionSort(int* ptrArr, int arrSize);
+void Shuffle(int* ptrArr, int arrSize, int shuffleCount);
+void SwapOnce(int* firstNumber, int* secondNumber);
+void ArrayFunc(int* ptrArr, int arrSize);
+void ArrayFunc2(int array_[], int arrSize);
+
+void SwapChar(char* firstChar, char* secondChar);
+void MovePlayer(char* mapArr_, int mapSize);
+void PrintMap(char* mapArr_, int mapSize);
+
+// 슬라이드 퍼플게임 구현해서 제출
+// 3 ~ 15 사이의 값을 입력받아서 해당 사이즈에 맞는 슬라이딩 퍼즐 구현(normal)
+// ex) 3 X 3 퍼즐
+// 클리어가 불가능한 퍼즐은 존재하지 않는다(very hard)
+
+
+void MovePlayer(char* mapArr_, int mapSize)
 {
-	int* minNum;
-	for (int i = 0; i < arrSize; i++)
+	const int MAX_ARR_ELEMENTS = 4;
+	const int MIN_ARR_ELEMENTS = 0;
+
+	//const int MAPSIZE = 25;
+	//char mapArr[MAPSIZE][MAPSIZE] = { 0, };
+
+	int xPosition = (mapSize - 1) / 2;
+	int yPosition = (mapSize - 1) / 2;
+
+
+	//mapArr[yPosition][xPosition] = '0';
+	*((mapArr_ + (yPosition * mapSize)) + xPosition) = '0';
+
+
+	PrintMap(mapArr_, mapSize);
+
+	char userInput = 0;
+
+	while (true)
 	{
-		minNum = ptrArr + i;
-		for (int j = i; j < arrSize; j++)
+		userInput = _getch();
+		system("cls");
+
+		if (userInput == 'a' || userInput == 'A')
 		{
-			if (ptrArr[j] < *minNum)
+			userInput = 0;
+
+			if (xPosition != MIN_ARR_ELEMENTS)
 			{
-				ShuffleOnce(minNum, ptrArr + j);
+				SwapChar(((mapArr_ + (yPosition * mapSize)) + xPosition), ((mapArr_ + (yPosition * mapSize)) + xPosition - 1));
+				xPosition--;
 			}
 		}
+		else if (userInput == 'd' || userInput == 'D')
+		{
+			userInput = 0;
+
+			if (xPosition != MAX_ARR_ELEMENTS)
+			{
+				SwapChar(((mapArr_ + (yPosition * mapSize)) + xPosition), ((mapArr_ + (yPosition * mapSize)) + xPosition + 1));
+				xPosition++;
+			}
+		}
+		else if (userInput == 'w' || userInput == 'W')
+		{
+			userInput = 0;
+
+			if (yPosition != MIN_ARR_ELEMENTS)
+			{
+				SwapChar(((mapArr_ + (yPosition * mapSize)) + xPosition), ((mapArr_ + (yPosition * mapSize) - 1) + xPosition));
+				yPosition--;
+			}
+		}
+		else if (userInput == 's' || userInput == 'S')
+		{
+			userInput = 0;
+
+			if (yPosition != MAX_ARR_ELEMENTS)
+			{
+				SwapChar(((mapArr_ + (yPosition * mapSize)) + xPosition), ((mapArr_ + (yPosition * mapSize) + 1) + xPosition));
+				yPosition++;
+			}
+		}
+		else if (userInput == 'q' || userInput == 'Q')
+		{
+			system("cls");
+			break;
+		}
+
+		PrintMap(mapArr_, mapSize);
+	}			// while()
+
+
+
+}
+
+void PrintMap(char* mapArr_, int mapSize)
+{
+	for (int y = 0; y < mapSize; y++)
+	{
+		for (int x = 0; x < mapSize; x++)
+		{
+			printf("%c ", *((mapArr_ + (y * mapSize)) + x) );
+		}
+		printf("\n");
 	}
 }
+
 
 int main()
 {
@@ -63,14 +151,14 @@ int main()
 
 	printf("numbers 의 Size : %d \n", sizeof(numbers));
 	// 포인터 복습
-	*/
-	int userInput = 0;
+	
+	// 정렬 실습
 	int numbers[5] = { 0, };
 	int* ptrNum = numbers;
+
 	for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
 	{
-		cin >> userInput;
-		numbers[i] = userInput;
+		cin >> numbers[i];
 	}
 
 	printf("\n셔플 전 \n");
@@ -79,13 +167,78 @@ int main()
 		printf("number[%d] 의 값 : %d \n", i, *(ptrNum + i));
 	}
 
-	SelecttionSort(numbers, sizeof(numbers) / sizeof(int));
+	// SelecttionSort(numbers, sizeof(numbers) / sizeof(int));
+	BubbleSort(numbers, sizeof(numbers) / sizeof(int));
+	// InsertionSort(numbers, sizeof(numbers) / sizeof(int));
 
 	printf("\n셔플 후 \n");
 	for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
 	{
 		printf("number[%d] 의 값 : %d \n", i, *(ptrNum + i));
 	}
+	// 정렬 실습
+
+	// 다차원 배열
+	char char1_[25] = { 0, };
+
+	for (int i = 0; i < 25; i++)
+	{
+		char1_[i] = '*';
+	}
+	//for (int i = 0; i < 25; i++)
+	//{
+	//	printf("%c ", char1_[i]);
+	//	if (i % 5 == 4)
+	//	{
+	//		printf("\n");
+	//	}
+	//}
+
+	printf("\n");
+
+	char char2_[5][5] = { 0, };
+	for (int y = 0; y < 5; y++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			if (x == 2 && y == 2)
+			{
+				char2_[y][x] = '0';
+			}
+			char2_[y][x] = '*';
+		}
+	}
+
+	for (int y = 0; y < 5; y++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			printf("%c ", char2_[y][x]);
+		}
+		printf("\n");
+	}
+	// 다차원 배열
+	*/
+
+	// char** mapArr2 = new char* [MAPSIZE];
+	char mapArr2[5][5] = {0, };
+	
+	for (int y = 0; y < 5; y++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			// printf("%c ", mapArr2[y][x]);
+			mapArr2[y][x] = '*';
+		}
+		printf("\n");
+	}
+
+
+	MovePlayer((char*)mapArr2, 5);
+
+	// PrintMap((char*)mapArr2, 5);
+
+
 
 
 	return 0;
@@ -100,17 +253,25 @@ void Shuffle(int* ptrArr, int arrSize, int shuffleCount)
 	{
 		randIdx1 = rand() % arrSize;
 		randIdx2 = rand() % arrSize;
-		ShuffleOnce((ptrArr + randIdx1), (ptrArr + randIdx2));
+		SwapOnce((ptrArr + randIdx1), (ptrArr + randIdx2));
 	}
 }
 
 // 포인터로 배열의 요소 두개를 섞는 함수
-void ShuffleOnce(int* firstNumber, int* secondNumber)
+void SwapOnce(int* firstNumber, int* secondNumber)
 {
 	int temp = 0;
 	temp = *firstNumber;
 	*firstNumber = *secondNumber;
 	*secondNumber = temp;
+}
+
+void SwapChar(char* firstChar, char* secondChar)
+{
+	int temp = 0;
+	temp = *firstChar;
+	*firstChar = *secondChar;
+	*secondChar = temp;
 }
 
 // int형 배열과 배열의 크기를 입력받아 배열의 처음부터 끝까지 출력하는 함수
@@ -126,9 +287,58 @@ void ArrayFunc(int* ptrArr, int arrSize)
 void ArrayFunc2(int array_[], int arrSize) 
 {
 	// 포인터 변수의 크기는 운영체계(64비트)와 같다 데이터를 불러올 수 있는 최소 단위
-	printf("함수에서 numbers 의 Size : %d \n", sizeof(array_));
+	//printf("함수에서 numbers 의 Size : %d \n", sizeof(array_));
 	for (int i = 0; i < arrSize; i++)
 	{
 		printf("ptrArr[%d] 역참조 값 : %d \n", i, array_[i]);
 	}
 }
+
+void SelecttionSort(int* ptrArr, int arrSize)
+{
+	int* minNum;
+	for (int i = 0; i < arrSize - 1; i++)
+	{
+		minNum = ptrArr + i;
+		for (int j = i; j < arrSize; j++)
+		{
+			if (*minNum > ptrArr[j])
+			{
+				minNum = &ptrArr[j];
+			}
+		}
+		SwapOnce(minNum, ptrArr + i);
+	}		// for() 종료
+}
+
+void BubbleSort(int* ptrArr, int arrSize)
+{
+	for (int j = 1; j < arrSize; j++)
+	{
+		for (int i = 0; (i + 1) < arrSize; i++)
+		{
+			// printf("돌고는 있냐\n");
+			if (*(ptrArr + i) > *(ptrArr + i + 1)) 
+			{
+				// printf("섞고는 있냐\n");
+				SwapOnce(ptrArr + i, ptrArr + i + 1);
+			}
+		}
+	}
+}
+
+void InsertionSort(int* ptrArr, int arrSize)
+{
+	for (int i = 1; i < arrSize; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (*(ptrArr + (i - j)) < *(ptrArr + (i - j - 1)))
+			{
+				// printf("섞었니? \n");
+				SwapOnce((ptrArr + (i - j)), (ptrArr + (i - j - 1)));
+			}
+		}
+	}
+}
+
